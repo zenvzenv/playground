ea61ff3961f4af8a1807e8d85fd8c20c3172e6fb
zw030301 <zw30522@live.com>
2019-05-03 15:17:31 +0800

    冒泡排序

---
+++ src/main/java/zhengwei/algorithm/BubbleSort.java
@@ -0,0 +1,24 @@
+package zhengwei.algorithm;
+
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ * 时间复杂度：O(n^2),空间复杂度:O(1),稳定
+ * 思想：冒泡排序的时间复杂度虽然和选择排序相同，但是在代码实际运行中，冒泡排序的效率会比选择排序的效率要高
+ * 因为冒泡排序每排完一次序之后，下次再要排序的数组长度会比上次一次的长度要短，
+ * 外循环来控制需要排序的数组的长度，内循环来遍历接下来需要比较的数组内容以比较大小。
+ * @author zhengwei AKA Sherlock
+ * @since 2019/4/27 9:31
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr=new int[]{1,5,3,4,6,7,8,1,2,3,9};
+        for (int i = arr.length-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j+1]) CommonUtils.swap(j+1, j, arr);
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
