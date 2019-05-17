d1dc272470917cd7f74b24da95e1987d4df5cd73
zw030301 <zw30522@live.com>
2019-05-17 17:30:34 +0800

    SparkWordCount优化

--- src/main/java/zhengwei/spark/SparkWordCount.java
+++ src/main/java/zhengwei/spark/SparkWordCount.java
@@ -5,9 +5,9 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
+import zhengwei.common.SysUtils;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author zhengwei AKA Sherlock
@@ -15,17 +15,18 @@ import java.util.List;
  */
 public class SparkWordCount {
 	public static void main(String[] args) {
+		if (SysUtils.isNull(args)) {
+			System.out.println("请输入要统计的文件路径");
+			return;
+		}
 		SparkConf conf=new SparkConf().setAppName("SparkWordCount").setMaster("local[2]");
 		JavaSparkContext sc=new JavaSparkContext(conf);
-		JavaRDD<String> lines = sc.textFile("src/main/resources/input/word.zw");
-		JavaRDD<String[]> map = lines.map(line->line.split(" "));
+		JavaRDD<String> lines = sc.textFile(args[0]);
+		JavaRDD<String[]> map = lines.map(line->line.split("[ ]"));
 		System.out.println(map.collect().toString());
-		JavaRDD<String> words = lines.flatMap(line->Arrays.asList(line.split(" ")).iterator());
+		JavaRDD<String> words = lines.flatMap(line->Arrays.asList(line.split("[ ]")).iterator());
 		JavaPairRDD<String, Integer> ones = words.mapToPair(word->new Tuple2<>(word, 1));
 		JavaPairRDD<String,Integer> count = ones.reduceByKey((x1,x2)->x1+x2);
-		List<Tuple2<String,Integer>> collect = count.collect();
-		for (Tuple2<String, Integer> tuple2 : collect) {
-			System.out.println(tuple2._1()+"---"+tuple2._2());
-		}
+		count.foreach(System.out::println);
 	}
 }
