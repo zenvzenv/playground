d6e04217c366ea947e9bc8c9ba37aec220a2949f
zw030301 <zw30522@live.com>
2019-06-03 19:19:22 +0800

    补充ClassLoader的JavaDoc

--- src/main/java/zhengwei/jvm/TestClassLoader3.java
+++ src/main/java/zhengwei/jvm/TestClassLoader3.java
@@ -1,5 +1,9 @@
 package zhengwei.jvm;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Objects;
+
 /**
  * @author zhengwei AKA Sherlock
  * @since 2019/5/29 10:16
@@ -18,6 +22,36 @@ public class TestClassLoader3 {
          */
         Class<?> clazz2=Class.forName("zhengwei.jvm.C");
         System.out.println(clazz2.getClassLoader());
+    }
+
+    /**
+     * JVM中类加载器的层次关系：Bootstrap ClassLoader->ExtClassLoader->AppClassLoader
+     * 在有些实现中BootstrapClassLoader会用null表示
+     */
+    @Test
+    void testParentClassLoader(){
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+        System.out.println(classLoader);
+        while (!Objects.isNull(classLoader)){
+            classLoader=classLoader.getParent();
+            System.out.println(classLoader);
+        }
+    }
+
+    /**
+     * CLassLoader JavaDoc
+     * 数组的class对象不是由ClassLoader创建的，而是由JVM在运行期间自动创建的，
+     */
+    @Test
+    void testArrayClassLoader(){
+        String[] strings=new String[2];
+        System.out.println(strings.getClass().getClassLoader());//根加载器->null
+        System.out.println("--------");
+        C[] cs=new C[2];
+        System.out.println(cs.getClass().getClassLoader());
+        System.out.println("--------");
+        int[] ints=new  int[2];
+        System.out.println(ints.getClass().getClassLoader());//没有classloader->null
     }
 }
 class C{
