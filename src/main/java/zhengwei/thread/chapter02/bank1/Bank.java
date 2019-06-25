96ed69e8cf6d2f1f58b9d4afb5d70b9c6d221ea9
zw030301 <zw30522@live.com>
2019-06-25 19:48:19 +0800

    多线程模拟银行叫号第一版

---
+++ src/main/java/zhengwei/thread/chapter02/bank1/Bank.java
@@ -0,0 +1,17 @@
+package zhengwei.thread.chapter02.bank1;
+
+/**
+ * 模拟银行叫号
+ * @author zhengwei AKA Sherlock
+ * @since 2019/6/25 19:23
+ */
+public class Bank {
+	public static void main(String[] args) {
+		TicketWindow ticketWindow1=new TicketWindow("一号柜台");
+		ticketWindow1.start();
+		TicketWindow ticketWindow2=new TicketWindow("二号柜台");
+		ticketWindow2.start();
+		TicketWindow ticketWindow3=new TicketWindow("三号柜台");
+		ticketWindow3.start();
+	}
+}
