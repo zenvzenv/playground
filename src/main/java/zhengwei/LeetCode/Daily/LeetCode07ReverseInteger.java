71a9d22e2ee3374a3d181aa24d2ff5ce90a7f956
zw030301 <zw30522@live.com>
2019-07-20 20:27:55 +0800

    LeetCode日常刷题第七题-翻转一个Integer

---
+++ src/main/java/zhengwei/LeetCode/Daily/LeetCode07ReverseInteger.java
@@ -0,0 +1,40 @@
+package zhengwei.LeetCode.Daily;
+
+/**
+ * LeetCode第七题
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 123
+ * 输出: 321
+ *  示例 2:
+ * <p>
+ * 输入: -123
+ * 输出: -321
+ * 示例 3:
+ * <p>
+ * 输入: 120
+ * 输出: 21
+ * 注意:
+ * <p>
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-integer
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author zhengwei AKA Awei
+ * @since 2019/7/20 20:17
+ */
+public class LeetCode07ReverseInteger {
+	public int reverse(int x) {
+		int result = 0;
+		while (x != 0) {
+			int newResult = result * 10 + x % 10;
+			if ((newResult - x % 10) / 10 != result) return 0;
+			x /= 10;
+		}
+		return result;
+	}
+}
