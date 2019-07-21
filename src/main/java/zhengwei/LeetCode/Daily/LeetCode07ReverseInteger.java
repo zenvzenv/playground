43e78524ff44a6648acd69daeef315902398b226
zw030301 <zw30522@live.com>
2019-07-21 18:02:46 +0800

    fix bug

--- src/main/java/zhengwei/LeetCode/Daily/LeetCode07ReverseInteger.java
+++ src/main/java/zhengwei/LeetCode/Daily/LeetCode07ReverseInteger.java
@@ -18,7 +18,7 @@ package zhengwei.LeetCode.Daily;
  * 输出: 21
  * 注意:
  * <p>
- * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
  * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/reverse-integer
@@ -32,7 +32,10 @@ public class LeetCode07ReverseInteger {
 		int result = 0;
 		while (x != 0) {
 			int newResult = result * 10 + x % 10;
+			//校验是否Integer越界
 			if ((newResult - x % 10) / 10 != result) return 0;
+			//更新result的值
+			result = newResult;
 			x /= 10;
 		}
 		return result;
