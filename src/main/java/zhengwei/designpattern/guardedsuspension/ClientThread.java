88c0706b0ba317373e52ec45e5c2441ee7b0b03e
zw030301 <zw30522@live.com>
2019-07-30 19:30:12 +0800

    GuardedSuspension设计模式，当我们的处理能力达到极限时，我们把新接受到的任务放入队列中，等我们时间去处理的时候再去从队列中取出任务进行处理

---
+++ src/main/java/zhengwei/designpattern/guardedsuspension/ClientThread.java
@@ -0,0 +1,33 @@
+package zhengwei.designpattern.guardedsuspension;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+/**
+ * @author zhengwei AKA Awei
+ * @since 2019/7/30 13:00
+ */
+public class ClientThread extends Thread {
+	private final RequestQueue queue;
+	private final Random random;
+	private final String sendValue;
+
+	public ClientThread(RequestQueue queue, String sendValue) {
+		this.queue = queue;
+		this.sendValue = sendValue;
+		this.random = new Random(System.currentTimeMillis());
+	}
+
+	@Override
+	public void run() {
+		IntStream.rangeClosed(1,10).forEach(i->{
+			System.out.println("Client - request -> "+sendValue);
+			queue.putRequest(new Request(sendValue));
+			try {
+				Thread.sleep(random.nextInt(1000));
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+	}
+}
---
+++ src/main/java/zhengwei/designpattern/guardedsuspension/RequestQueue.java
@@ -0,0 +1,42 @@
+package zhengwei.designpattern.guardedsuspension;
+
+import java.util.LinkedList;
+
+/**
+ * @author zhengwei AKA Awei
+ * @since 2019/7/30 12:53
+ */
+public class RequestQueue {
+	private static final LinkedList<Request> queue = new LinkedList<>();
+
+	/**
+	 * 我们有时间去处理了，去队列中取出任务来处理
+	 *
+	 * @return 需要处理的请i去
+	 */
+	public Request getRequest() {
+		synchronized (queue) {
+			while (queue.size() <= 0) {
+				try {
+					queue.wait();
+				} catch (InterruptedException e) {
+//					e.printStackTrace();
+					return null;
+				}
+			}
+			return queue.removeFirst();
+		}
+	}
+
+	/**
+	 * 把请求放到队列中去，等到我们有时间了再去处理它
+	 *
+	 * @param request 需要处理的请求
+	 */
+	public void putRequest(Request request) {
+		synchronized (queue) {
+			queue.addLast(request);
+			queue.notifyAll();
+		}
+	}
+}
