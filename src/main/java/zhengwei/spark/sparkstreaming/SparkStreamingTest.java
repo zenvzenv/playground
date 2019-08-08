e4a79ed5733eb7d637b32899b6b0dcd6fcd0eb33
zw030301 <zw30522@live.com>
2019-08-08 18:15:03 +0800

    SparkStreaming整合kafka小案例

--- src/main/java/zhengwei/spark/sparkstreaming/SparkStreamingTest.java
+++ src/main/java/zhengwei/spark/sparkstreaming/SparkStreamingTest.java
@@ -1,17 +1,18 @@
 package zhengwei.spark.sparkstreaming;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaPairDStream;
-import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
-import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import org.apache.spark.streaming.api.java.*;
+import org.apache.spark.streaming.kafka010.ConsumerStrategies;
+import org.apache.spark.streaming.kafka010.KafkaUtils;
+import org.apache.spark.streaming.kafka010.LocationStrategies;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import scala.Tuple2;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * @author zhengwei AKA Awei
@@ -35,6 +36,29 @@ public class SparkStreamingTest {
 		JavaDStream<String> words = lines.flatMap(line -> Arrays.asList(line.split("[ ]")).iterator());
 		JavaPairDStream<String, Integer> pairs = words.mapToPair(word -> new Tuple2<>(word, 1));
 		JavaPairDStream<String, Integer> result = pairs.reduceByKey(Integer::sum);
+		result.print();
+		jssc.start();
+		jssc.awaitTermination();
+	}
+
+	@Test
+	void sparkStreamingKafkaWordCount() throws InterruptedException {
+		Collection<String> topic = new HashSet<>();
+		topic.add("test");
+		Map<String, Object> kafkaParam = new HashMap<>();
+		kafkaParam.put("group.id", "test-consumer-group");
+		kafkaParam.put("bootstrap.servers", "10.21.17.121:9092");
+		kafkaParam.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		kafkaParam.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+		kafkaParam.put("auto.offset.reset", "earliest");
+		JavaInputDStream<ConsumerRecord<String, String>> kafkaStream = KafkaUtils.createDirectStream(
+				jssc,
+				LocationStrategies.PreferConsistent(),
+				ConsumerStrategies.Subscribe(topic, kafkaParam)
+		);
+		JavaDStream<String> lines = kafkaStream.map(ConsumerRecord::value);
+		JavaPairDStream<String, Integer> wordAndOne = lines.mapToPair(line -> new Tuple2<>(line.split("[ ]")[0], 1));
+		JavaPairDStream<String, Integer> result = wordAndOne.reduceByKey(Integer::sum);
 		result.print();
 		jssc.start();
 		jssc.awaitTermination();
