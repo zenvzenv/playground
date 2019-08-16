2156d23fcdd5f2df688ff97705bf7149cbeba5ce
zw030301 <zw30522@live.com>
2019-08-16 22:54:14 +0800

    添加一些常用的shell命令

--- src/main/shell/common_shell.sh
+++ src/main/shell/common_shell.sh
@@ -13,3 +13,18 @@ spark-submit --class org.apache.spark.examples.JavaSparkPi --master yarn-client 
 yarn log -applicationId xxx
 #获取正在运行的application
 yarn application -list -appStates RUNNING
+#yarn查看队列的资源状态
+yarn queue -status root.user.user
+#计算时间
+date -d "7 day ago" +"%Y/%m/%d/%H/%M"
+#对于给定的时间来计算时间
+date -d "20190718 2 day ago" +"%Y%m%d%H%M"
+#修改文件字符集
+iconv -f {转换前字符集} -t {转换后的字符集} inputfile -o outputfile
+iconv -f utf-8 -t gb18030 a.txt -o b.txt
+#查看最大深度1的文件大小
+du -h --max-depth=1
+#tar压缩
+tar czvf FileName.tar DirName
+#tar解压
+tar zxvf FileName.tar
