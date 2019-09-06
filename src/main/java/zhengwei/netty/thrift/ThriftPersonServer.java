9c95619cf89abba1a9897e2c831cda5c4ba0f4bb
zw030301 <zw30522@live.com>
2019-09-06 09:17:31 +0800

    fix bug

--- src/main/java/zhengwei/netty/thrift/ThriftPersonServer.java
+++ src/main/java/zhengwei/netty/thrift/ThriftPersonServer.java
@@ -1,11 +1,11 @@
 package zhengwei.netty.thrift;
 
 
+import org.apache.thrift.TProcessorFactory;
 import org.apache.thrift.protocol.TCompactProtocol;
-import org.apache.thrift.protocol.TJSONProtocol;
 import org.apache.thrift.server.THsHaServer;
 import org.apache.thrift.server.TServer;
-import org.apache.thrift.transport.TFastFramedTransport;
+import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TNonblockingServerSocket;
 import org.apache.thrift.transport.TTransportException;
 
@@ -20,9 +20,11 @@ public class ThriftPersonServer {
 		TNonblockingServerSocket socket = new TNonblockingServerSocket(8888);
 		THsHaServer.Args arg = new THsHaServer.Args(socket).minWorkerThreads(2).maxWorkerThreads(4);
 		PersonService.Processor<ThriftPersonServiceImpl> processor = new PersonService.Processor<>(new ThriftPersonServiceImpl());
+		//协议层
 		arg.protocolFactory(new TCompactProtocol.Factory());
-		arg.transportFactory(new TFastFramedTransport.Factory());
-		arg.protocolFactory(new TJSONProtocol.Factory());
+		//传输层
+		arg.transportFactory(new TFramedTransport.Factory());
+		arg.processorFactory(new TProcessorFactory(processor));
 
 		TServer server = new THsHaServer(arg);
 		System.out.println("Thrift Server Started");
