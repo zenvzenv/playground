c82b5f1f61054ec5336665be0814118258399be1
zw030301 <zw30522@live.com>
2019-09-27 21:04:29 +0800

    shell函数库，新增获取文件名和获取文件路径的函数，以替代外部函数basename和dirname

--- src/main/shell/shell_lib.sh
+++ src/main/shell/shell_lib.sh
@@ -2,11 +2,44 @@
 #平常会用到的一些Shell函数，在此总结下，方便以后复用
 #会持续更新
 #find BIN_HOME and APP_HOME
-function findBinHome() {
+findBinHome() {
     SHELL_NAME=${0}
     BIN_HOME=$(dirname "${0}")
     #BIN_HOME path
     BIN_HOME=$(cd "${BIN_HOME}";pwd)
     #APP_HOME path
     APP_HOME=$(dirname "${BIN_HOME}")
+}
+#获取文件名，取代basename
+#Useage
+#$ basename ~/Pictures/Wallpapers/1.jpg
+#1.jpg
+#$ basename ~/Pictures/Wallpapers/1.jpg .jpg
+#1
+#$ basename ~/Pictures/Downloads/
+#Downloads
+basename() {
+    # Usage: basename "path" ["suffix"]
+    local tmp
+    tmp=${1%"${1##*[!/]}"}
+    tmp=${tmp##*/}
+    tmp=${tmp%"${2/"$tmp"}"}
+    printf '%s\n' "${tmp:-/}"
+}
+#获得目录，取代dirname
+dirname() {
+    # Usage: dirname "path"
+    local tmp=${1:-.}
+    [[ ${tmp} != *[!/]* ]] && {
+        printf '/\n'
+        return
+    }
+    tmp=${tmp%%"${tmp##*[!/]}"}
+    [[ ${tmp} != */* ]] && {
+        printf '.\n'
+        return
+    }
+    tmp=${tmp%/*}
+    tmp=${tmp%%"${tmp##*[!/]}"}
+    printf '%s\n' "${tmp:-/}"
 }
