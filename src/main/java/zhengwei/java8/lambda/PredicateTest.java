e3ebc2734293b626a367d2c88bd82c95e92d9557
zhengwei <zw030301@live.com>
2019-10-17 21:19:22 +0800

    Predicate函数式接口

--- src/main/java/zhengwei/java8/lambda/PredicateTest.java
+++ src/main/java/zhengwei/java8/lambda/PredicateTest.java
@@ -10,72 +10,107 @@ import java.util.function.Predicate;
  * @author zhengwei AKA Awei
  * @since 2019/9/25 13:42
  */
-public class PredicateTest {
-	private static final List<Integer> LIST = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+class PredicateTest {
+    private static final List<Integer> LIST = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-	public static void main(String[] args) {
-		Predicate<String> predicate = s -> s.length() > 5;
-		System.out.println(predicate.test("hello"));
-	}
+    static void main(String[] args) {
+        Predicate<String> predicate = s -> s.length() > 5;
+        System.out.println(predicate.test("hello"));
+    }
 
-	@Test
-	void testPredicate() {
+    @Test
+    void testPredicate() {
 
-		//所有方法的实现全部由使用者来提供
-		conditionFilter(LIST, integer -> integer % 2 == 0);
-		conditionFilter(LIST, integer -> integer % 2 != 0);
-		conditionFilter(LIST, integer -> integer > 5);
-		conditionFilter(LIST, integer -> integer < 3);
-		conditionFilter(LIST, integer -> true);
-		conditionFilter(LIST, integer -> false);
-	}
+        //所有方法的实现全部由使用者来提供
+        conditionFilter(LIST, integer -> integer % 2 == 0);
+        conditionFilter(LIST, integer -> integer % 2 != 0);
+        conditionFilter(LIST, integer -> integer > 5);
+        conditionFilter(LIST, integer -> integer < 3);
+        conditionFilter(LIST, integer -> true);
+        conditionFilter(LIST, integer -> false);
+    }
 
-	@Test
-	void testPredicateAnd() {
-		conditionAndFilter(LIST,
-				integer -> integer % 2 == 0,
-				integer -> integer > 5);
-	}
+    @Test
+    void testPredicateAnd() {
+        conditionAndFilter(LIST,
+                integer -> integer % 2 == 0,
+                integer -> integer > 5);
+    }
 
-	private static void conditionFilter(List<Integer> list, Predicate<Integer> predicate) {
-		list.forEach(x -> {
-			if (predicate.test(x)) System.out.printf("int:%d\t", x);
-		});
-		System.out.println();
-	}
+    private static void conditionFilter(List<Integer> list, Predicate<Integer> predicate) {
+        list.forEach(x -> {
+            if (predicate.test(x)) System.out.printf("int:%d\t", x);
+        });
+        System.out.println();
+    }
+
+    /**
+     * 两个Predicate函数的and操作
+     *
+     * @param list       待操作集合
+     * @param predicate1 第一个Predicate
+     * @param predicate2 第二个Predicate
+     */
+    private static void conditionAndFilter(List<Integer> list,
+                                           Predicate<Integer> predicate1,
+                                           Predicate<Integer> predicate2) {
+        list.forEach(x -> {
+            //and的操作，即同时满足两个Predicate都返回true才会去执行接下来的操作
+            if (predicate1.and(predicate2).test(x)) {
+                System.out.println(x);
+            }
+        });
+    }
+
+    /**
+     * 两个Predicate函数的or操作
+     *
+     * @param list       待操作集合
+     * @param predicate1 第一个Predicate
+     * @param predicate2 第二个Predicate
+     */
+    private static void conditionOrFilter(List<Integer> list,
+                                          Predicate<Integer> predicate1,
+                                          Predicate<Integer> predicate2) {
+        list.forEach(x -> {
+            if (predicate1.or(predicate2).test(x)) {
+                System.out.println(x);
+            }
+        });
+    }
+
+    /**
+     * Predicate取反操作
+     *
+     * @param list      代操作集合
+     * @param predicate 判断函数
+     */
+    private static void conditionAndNagate(List<Integer> list, Predicate<Integer> predicate) {
+        list.forEach(x -> {
+            if (predicate.negate().test(x)) System.out.println(x);
+        });
+    }
+
+    /**
+     * 用来判断两个参数是否相等
+     * 最终是由Objects中的
+     *
+     * @param object 需要判断的值
+     * @return true-相等，false-不相等
+     */
+    static Predicate<String> isEqual(Object object) {
+        return Predicate.isEqual(object);
+    }
 
 	/**
-	 * 两个Predicate函数的and操作
-	 *
-	 * @param list       待操作集合
-	 * @param predicate1 第一个Predicate
-	 * @param predicate2 第二个Predicate
+	 * 后面的那个test参数相当于传进去的object
 	 */
-	private static void conditionAndFilter(List<Integer> list,
-	                                       Predicate<Integer> predicate1,
-	                                       Predicate<Integer> predicate2) {
-		list.forEach(x -> {
-			//and的操作，即同时满足两个Predicate都返回true才会去执行接下来的操作
-			if (predicate1.and(predicate2).test(x)) {
-				System.out.println(x);
-			}
-		});
-	}
+    static Predicate isEqual = object -> object.equals("test");
 
-	/**
-	 * 两个Predicate函数的or操作
-	 *
-	 * @param list       待操作集合
-	 * @param predicate1 第一个Predicate
-	 * @param predicate2 第二个Predicate
-	 */
-	private static void conditionOrFilter(List<Integer> list,
-	                                      Predicate<Integer> predicate1,
-	                                      Predicate<Integer> predicate2) {
-		list.forEach(x -> {
-			if (predicate1.or(predicate2).test(x)) {
-				System.out.println(x);
-			}
-		});
+    @Test
+    void testIsEqual() {
+    	//等价于下面的那种形式
+        System.out.println(isEqual("test").test("test"));
+		System.out.println(isEqual.test("test"));
 	}
 }
