c631b70c029d50f435a55471a592a1c00e48331f
zhengwei <zw30522@live.com>
2019-12-18 17:38:06 +0800

    iot shell

--- src/main/shell/js_iot/rm_one_day_ago_dir.sh
+++ src/main/shell/js_iot/rm_one_day_ago_dir.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
+if [[ $# -lt 1 ]]; then
+    echo "enter statengine or poor"
+    exit
+fi
+type=$1
 cd $(dirname $0)
 BIN_HOME=$(pwd)
 . ${BIN_HOME}/conf.sh
 . conf.sh
 delay="-1 day"
-rm_date=$(date -d "${delay}" +"%Y/%m/%d/%H")
+rm_date=""
 rm_cmd=$(which rm)
-while read table_name
-do
-    echo "table name -> ${table_name%_*}"
-    echo "will rm ${DATA_HOME}/${table_name%_*}/${rm_date}"
-    ${rm_cmd} -r ${DATA_HOME}/${table_name%_*}/${rm_date}
-done < ${CONF_HOME}/table_name.conf
+if [[ "statengine" == ${type} ]]; then
+    rm_date=$(date -d "${delay}" +"%Y/%m/%d/%H")
+    DATA_HOME="${DATA_HOME}/dataimport/cp/statcenter"
+elif [[ "poor" == ${type} ]]; then
+    rm_date=$(date -d "${delay}" +"%Y/%m/%d/%H/%M")
+    DATA_HOME="${DATA_HOME}/fromoracle/poor"
+else
+    exit
+fi
+echo "will rm ${DATA_HOME}/${rm_date}"
+${rm_cmd} -r "${DATA_HOME}/${rm_date}"
