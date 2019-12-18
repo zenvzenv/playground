bd0c15ab4b07892834bc76318e2ba27f16ef7b51
zhengwei <zw030301@live.com>
2019-12-18 20:50:15 +0800

    Future cancel

--- thread/src/main/java/zhengwei/thread/future/FutureDemo1.java
+++ thread/src/main/java/zhengwei/thread/future/FutureDemo1.java
@@ -7,8 +7,11 @@ import java.util.concurrent.*;
  * @since 2019/12/18 12:50
  */
 public class FutureDemo1 {
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        testGet();
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+//        testGet();
+//        testGetTimeout();
+//        testCancel();
+        testCancel2();
     }
 
     /**
@@ -23,10 +26,12 @@ public class FutureDemo1 {
                 e.printStackTrace();
             }
         });
+        //main thread
         final Thread callerThread = Thread.currentThread();
         new Thread(() -> {
             try {
                 TimeUnit.SECONDS.sleep(1);
+                //打断主线程，主线程在get的时候阻塞住了，打断它将不会去获取值了
                 callerThread.interrupt();
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -41,7 +46,32 @@ public class FutureDemo1 {
         System.out.println(result);
     }
 
-    private static void testIsDone() {
+    /**
+     * {@link Future#get(long time, TimeUnit unit)}
+     */
+    private static void testGetTimeout() throws InterruptedException, ExecutionException, TimeoutException {
+        final ExecutorService executorService = Executors.newCachedThreadPool();
+        final Future<?> future = executorService.submit(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+                System.out.println("===========");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        //虽然get的时候timeout了，到了指定的时间超时了，但是线程池中的线程依然会去执行
+        final Object result = future.get(5, TimeUnit.SECONDS);
+        System.out.println(result);
+    }
+
+    /**
+     * {@link Future#isDone()}
+     * Future会在一下几种情况是done状态
+     * normal termination 正常结束
+     * exception 出现异常
+     * cancellation 被cancel
+     */
+    private static void testIsDone() throws ExecutionException, InterruptedException {
         final ExecutorService executorService = Executors.newFixedThreadPool(5);
         final Future<?> future = executorService.submit(() -> {
             try {
@@ -50,6 +80,66 @@ public class FutureDemo1 {
                 e.printStackTrace();
             }
         });
+        System.out.println(future.get());
+        boolean done = future.isDone();
+        System.out.println(done);
+    }
+
+    /**
+     * {@link Future#cancel(boolean mayInterruptIfRunning)}
+     * cancel会在一下集中情况下失败
+     * 1. task已经执行完毕了
+     * 2. 这个task已经被cancel过一次了
+     * <p>
+     * cancel一般配合Thread.interrupted()使用
+     *
+     * 在task被取消成功之后，如果再去get该future的值时，将会报CancellationException，不再让我们获取该task的值了
+     */
+    private static void testCancel() throws ExecutionException, InterruptedException {
+        final ExecutorService executorService = Executors.newFixedThreadPool(5);
+        final Future<Integer> future = executorService.submit(() -> {
+            //cancel不了，没有地方执行中断
+            /*while (true) {
+
+            }*/
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            /*while (!Thread.interrupted()) {
+                System.out.println("I am not be canceled");
+            }*/
+            System.out.println("I will be out");
+            return 10;
+        });
+        //会去打断线程
+        System.out.println(future.cancel(true));
+        //已经被cancel过一次了，再cancel就会失败
+        System.out.println(future.cancel(true));
+        System.out.println(future.isDone());
+        System.out.println(future.isCancelled());
+        //cancel之后将不会再获取到结果
+//        System.out.println(future.get());
+    }
+
+    /**
+     * 就算task被cancel了之后，但是线程还是会继续向下执行，但是最终的结果我们将获取不到了
+     */
+    private static void testCancel2() throws InterruptedException {
+        final ExecutorService executorService = Executors.newFixedThreadPool(5);
+        Future<Integer> future = executorService.submit(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+                System.out.println("=================");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("1111111111111111");
+            return 10;
+        });
+        TimeUnit.MILLISECONDS.sleep(10);
+        future.cancel(true);
 
     }
 }
