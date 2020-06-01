45ea5aa89fcbd4da64b2e9bfbf859789e71a132e
zhengwei <zw30522@live.com>
2020-06-01 10:46:13 +0800

    创建例外表的天目录

--- src/main/shell/js_iot/create_day_dir.sh
+++ src/main/shell/js_iot/create_day_dir.sh
@@ -43,3 +43,18 @@ for (( hour = 0; hour < 24; hour++ )); do
         done
     fi
 done
+#create special table day dir
+special=("it_gb_sign_15m" "it_lac_sign_15m" "it_s1_user_15m" "it_terminal_tac_sign" "it_lacci_user_15m" "it_s1_mme_sign_15m" "it_serip_user_15m" "it_terminal_tac_user")
+for tableName in ${special[@]};
+do
+    special_path="${DATA_HOME}/dataimport/cp/statcenter/${tableName}/${dir_path_time}/"
+    for (( hour = 0; hour < 24; hour++ )); do
+        if [[ ${hour} -lt 10 ]]; then
+    #            echo "${new_path}/0${hour}"
+            ${mkdir_cmd} -p "${special_path}/0${hour}"
+        else
+    #            echo "${new_path}/${hour}"
+            ${mkdir_cmd} -p "${special_path}/${hour}"
+        fi
+    done
+done
