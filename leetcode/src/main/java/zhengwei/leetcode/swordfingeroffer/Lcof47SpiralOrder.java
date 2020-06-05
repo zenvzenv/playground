103221e79c37db10acfe4ad9d1958e03b99c3616
zhengwei <zw30522@live.com>
2020-06-05 21:59:29 +0800

    剑指offer第47题:顺时针遍历二维数组

---
+++ leetcode/src/main/java/zhengwei/leetcode/swordfingeroffer/Lcof47SpiralOrder.java
@@ -0,0 +1,44 @@
+package zhengwei.leetcode.swordfingeroffer;
+
+import java.util.Arrays;
+
+/**
+ * 剑指offer第47题
+ * 顺时针遍历二维数组
+ *
+ * @author zhengwei AKA Awei
+ * @since 2020/6/5 15:12
+ */
+public class Lcof47SpiralOrder {
+    private static int[] spiralOrder(int[][] matrix) {
+        int top = 0;
+        int bottom = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int index = 0;
+        int[] res = new int[(bottom + 1) * (right + 1)];
+        while (true) {
+            //从左往右
+            for (int i = left; i <= right; i++) res[index++] = matrix[top][i];
+            top++;
+            if (top > bottom) break;
+            //从上到下
+            for (int i = top; i <= bottom; i++) res[index++] = matrix[i][right];
+            right--;
+            if (right < left) break;
+            //从右到左
+            for (int i = right; i >= left; i--) res[index++] = matrix[bottom][i];
+            bottom--;
+            if (bottom < top) break;
+            //从下到上
+            for (int i = bottom; i >= top; i--) res[index++] = matrix[i][left];
+            left++;
+            if (left > right) break;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(spiralOrder(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}})));
+    }
+}
