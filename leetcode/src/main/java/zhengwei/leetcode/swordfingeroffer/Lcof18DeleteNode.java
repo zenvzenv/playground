c5806c3f3c99c3c079397684587b079549029224
zhengwei <zw30522@live.com>
2020-06-30 20:22:34 +0800

    抽取出ListNode

--- leetcode/src/main/java/zhengwei/leetcode/swordfingeroffer/Lcof18DeleteNode.java
+++ leetcode/src/main/java/zhengwei/leetcode/swordfingeroffer/Lcof18DeleteNode.java
@@ -7,23 +7,6 @@ package zhengwei.leetcode.swordfingeroffer;
  * @since 2020/6/30 18:37
  */
 public class Lcof18DeleteNode {
-    private static final class ListNode {
-        final int val;
-        ListNode next;
-
-        public ListNode(int val) {
-            this.val = val;
-        }
-
-        @Override
-        public String toString() {
-            return "ListNode{" +
-                    "val=" + val +
-                    ", next=" + next +
-                    '}';
-        }
-    }
-
     public static ListNode deleteNode(ListNode head, int val) {
         if (null == head) return null;
         if (head.val == val) return head.next;
@@ -38,10 +21,7 @@ public class Lcof18DeleteNode {
     }
 
     public static void main(String[] args) {
-        ListNode root = new ListNode(4);
-        root.next = new ListNode(5);
-        root.next.next = new ListNode(1);
-        root.next.next.next = new ListNode(9);
+        ListNode root = ListNode.genListNode(new int[]{4, 5, 1, 9});
         System.out.println(deleteNode(root, 5));
     }
 }
