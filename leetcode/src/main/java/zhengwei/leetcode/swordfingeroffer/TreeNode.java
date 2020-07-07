c189182a31f383bcf03af9d10aaf684faf855e39
zhengwei <zw30522@live.com>
2020-07-07 19:58:03 +0800

    LeetCode需要用到的通用二叉树节点类

---
+++ leetcode/src/main/java/zhengwei/leetcode/swordfingeroffer/TreeNode.java
@@ -0,0 +1,15 @@
+package zhengwei.leetcode.swordfingeroffer;
+
+/**
+ * @author zhengwei AKA Awei
+ * @since 2020/7/7 19:27
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
