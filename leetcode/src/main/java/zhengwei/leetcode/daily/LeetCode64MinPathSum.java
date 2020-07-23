cd46f93bdcfbec0a2eb4869fdb0354a055193cb3
zhengwei <zw30522@live.com>
2020-07-23 19:08:48 +0800

    LeetCode64题最小路径和

---
+++ leetcode/src/main/java/zhengwei/leetcode/daily/LeetCode64MinPathSum.java
@@ -0,0 +1,28 @@
+package zhengwei.leetcode.daily;
+
+/**
+ * LeetCode64题最小路径和
+ *
+ * @author zhengwei AKA Awei
+ * @since 2020/7/23 19:07
+ */
+public class LeetCode64MinPathSum {
+    public int minPathSum(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int[][] dp = new int[rows][cols];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < rows; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < cols; i++) {
+            dp[0][i] = dp[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                dp[i][j] = Math.min(grid[i][j] + dp[i - 1][j], grid[i][j] + dp[i][j - 1]);
+            }
+        }
+        return dp[rows - 1][cols - 1];
+    }
+}
