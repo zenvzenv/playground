6dacfd09f1e629c95dc856a38fd85f60cf75c948
zhengwei <zw30522@live.com>
2020-07-30 10:14:22 +0800

    LeetCode第343题：整数拆分的最大乘积，新增数学解法

--- leetcode/src/main/java/zhengwei/leetcode/daily/LeetCode343IntegerBreak.java
+++ leetcode/src/main/java/zhengwei/leetcode/daily/LeetCode343IntegerBreak.java
@@ -24,6 +24,16 @@ public class LeetCode343IntegerBreak {
         return dp[n];
     }
 
+    //这于减绳子类似，尽量取3的幂
+    public static int integerBreak2(int n) {
+        if (n <= 3) return n - 1;
+        int remainder = n % 3;
+        int quotient = n / 3;
+        if (remainder == 0) return (int) Math.pow(3, quotient);
+        if (remainder == 1) return (int) Math.pow(3, quotient - 1) * 4;
+        else return (int) Math.pow(3, quotient) * 2;
+    }
+
     public static void main(String[] args) {
         System.out.println(integerBreak(3));
     }
