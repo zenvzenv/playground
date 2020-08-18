338340d49eb487cc7df7fb3cb309301c809bd383
zhengwei <zw30522@live.com>
2020-08-18 20:13:35 +0800

    LeetCode刷题需要用到的公用ListNode对象

---
+++ leetcode/src/main/java/zhengwei/leetcode/daily/ListNode.java
@@ -0,0 +1,36 @@
+package zhengwei.leetcode.daily;
+
+/**
+ * 刷题需要用到的公用对象
+ *
+ * @author zhengwei AKA Awei
+ * @since 2020/6/30 19:16
+ */
+public final class ListNode {
+    final int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    @Override
+    public String toString() {
+        return "ListNode{" +
+                "val=" + val +
+                ", next=" + next +
+                '}';
+    }
+
+    public static ListNode genListNode(int[] arr) {
+        ListNode root = new ListNode(arr[0]);
+        ListNode temp = root;
+        for (int i = 1; i < arr.length; i++) {
+            temp.next = new ListNode(arr[i]);
+            temp = temp.next;
+        }
+        return root;
+    }
+}
