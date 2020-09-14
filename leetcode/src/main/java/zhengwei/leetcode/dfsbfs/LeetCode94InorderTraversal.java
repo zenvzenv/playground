486b161781387af78b1317204b5aa870159fa4bb
zhengwei <zw30522@live.com>
2020-09-14 09:46:47 +0800

    94. 二叉树的中序遍历

---
+++ leetcode/src/main/java/zhengwei/leetcode/dfsbfs/LeetCode94InorderTraversal.java
@@ -0,0 +1,41 @@
+package zhengwei.leetcode.dfsbfs;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 94. 二叉树的中序遍历
+ *
+ * @author zhengwei AKA Awei
+ * @since 2020/9/14 9:35
+ */
+public class LeetCode94InorderTraversal {
+    public List<Integer> inorderTraversal1(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+        }
+        return res;
+    }
+
+    public List<Integer> inorderTraversalDFS(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        dfs(root, res);
+        return res;
+    }
+
+    private void dfs(TreeNode node, List<Integer> res) {
+        if (node == null) return;
+        dfs(node.left, res);
+        res.add(node.val);
+        dfs(node.right, res);
+    }
+}
