b7509b7e3555eb2c52fc67b624bac747a5b801ef
zhengwei <zw30522@live.com>
2020-12-10 09:28:08 +0800

    860. 柠檬水找零

---
+++ leetcode/src/main/java/zhengwei/leetcode/daily/LeetCode860LemonadeChange.java
@@ -0,0 +1,32 @@
+package zhengwei.leetcode.daily;
+
+/**
+ * 860. 柠檬水找零
+ *
+ * @author zhengwei AKA zenv
+ * @since 2020/12/10 9:16
+ */
+public class LeetCode860LemonadeChange {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
