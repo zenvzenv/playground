d9544cdf02b33dee2df22606d6cbc76d1935fa10
zhengwei <zw30522@live.com>
2020-12-21 17:32:50 +0800

    LeetCode第746题：使用最小的花费爬楼梯，dp 的另一种写法

--- leetcode/src/main/java/zhengwei/leetcode/daily/LeetCode746MinCostClimbingStairs.java
+++ leetcode/src/main/java/zhengwei/leetcode/daily/LeetCode746MinCostClimbingStairs.java
@@ -24,6 +24,17 @@ public class LeetCode746MinCostClimbingStairs {
         return dp[cost.length + 2];
     }
 
+    public int minCostClimbingStairs2(int[] cost) {
+        final int length = cost.length;
+        int[] dp = new int[length + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i <= length; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[length];
+    }
+
     public static void main(String[] args) {
         System.out.println(minCostClimbingStairs(new int[]{10, 15, 20}));
     }
