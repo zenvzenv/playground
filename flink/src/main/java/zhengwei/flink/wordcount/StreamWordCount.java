358466143ab8b5a11dc7c92904fb927f4cbe24b8
zenv <zw30522@live.com>
2021-01-26 20:30:12 +0800

    针对 Flink 1.12.x 新版本的一些调整

--- flink/src/main/java/zhengwei/flink/wordcount/StreamWordCount.java
+++ flink/src/main/java/zhengwei/flink/wordcount/StreamWordCount.java
@@ -1,5 +1,6 @@
 package zhengwei.flink.wordcount;
 
+import org.apache.flink.api.common.RuntimeExecutionMode;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
@@ -8,6 +9,10 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 public class StreamWordCount {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        //在 Flink 1.12.x 中已经将流批统一了
+        //只需在 env 中设置下运行模式即可
+        //automatic 会根据数据源自动推测出是批处理还是流处理
+        env.setRuntimeMode(RuntimeExecutionMode.AUTOMATIC);
         final ParameterTool parameterTool = ParameterTool.fromArgs(args);
         env
                 .socketTextStream(parameterTool.get("hostname"), parameterTool.getInt("port"))
@@ -17,7 +22,7 @@ public class StreamWordCount {
                         collector.collect(Tuple2.of(word, 1));
                     }
                 })
-                .keyBy(0)
+                .keyBy(tuple2->tuple2.f0)
                 .sum(1);
         env.execute();
     }
