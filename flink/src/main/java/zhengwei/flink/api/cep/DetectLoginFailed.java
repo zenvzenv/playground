034be17246d51e40956b682f89256bc8a29a8106
zenv <zw30522@live.com>
2021-04-29 17:24:41 +0800

    修改 CEP 示例代码，以确保示例可以运行

--- flink/src/main/java/zhengwei/flink/api/cep/DetectLoginFailed.java
+++ flink/src/main/java/zhengwei/flink/api/cep/DetectLoginFailed.java
@@ -1,14 +1,18 @@
 package zhengwei.flink.api.cep;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.cep.CEP;
 import org.apache.flink.cep.PatternSelectFunction;
 import org.apache.flink.cep.PatternStream;
 import org.apache.flink.cep.pattern.Pattern;
-import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.cep.pattern.conditions.SimpleCondition;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -18,29 +22,37 @@ import java.util.concurrent.TimeUnit;
  */
 public class DetectLoginFailed {
     @Data
+    @AllArgsConstructor
     final static class LoginEvent {
         private String userId;
         private String status;
-        private long ts = System.currentTimeMillis();
-
-        public LoginEvent(String userId, String status) {
-            this.userId = userId;
-            this.status = status;
-        }
+        private long ts;
     }
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
-        final DataStream<LoginEvent> source = env.socketTextStream("localhost", 8888)
+        final SingleOutputStreamOperator<LoginEvent> source = env.socketTextStream("localhost", 8888)
                 .map(line -> {
                     final String[] fields = line.split("[,]");
                     final String userId = fields[0];
                     final String status = fields[1];
-                    return new LoginEvent(userId, status);
-                });
+                    return new LoginEvent(userId, status, System.currentTimeMillis());
+                })
+                .assignTimestampsAndWatermarks(
+                        WatermarkStrategy.<LoginEvent>forBoundedOutOfOrderness(Duration.ofSeconds(2))
+                                .withTimestampAssigner((LoginEvent e, long recordTime) -> e.getTs())
+                );
+
+        source.print();
 
         final Pattern<LoginEvent, LoginEvent> pattern = Pattern.<LoginEvent>begin("failEvents")
+                .where(new SimpleCondition<LoginEvent>() {
+                    @Override
+                    public boolean filter(LoginEvent value) {
+                        return value.status.equals("failed");
+                    }
+                })
                 .times(3)//若不带 consecutive 的话，则默认不是严格临近
                 .consecutive()//表示严格临近
                 .within(Time.of(10, TimeUnit.SECONDS));
