53afeb8f6fc11a776a8f1b5d1ca10e28688fb1fd
zhengwei <zw030301@live.com>
2021-05-09 21:26:05 +0800

    ANTLR v4 监听器操作语法初始化数组

---
+++ antlr/src/main/java/zhengwei/antlr/arrayinit/ArrayInitListener.java
@@ -0,0 +1,30 @@
+// Generated from I:/MyWorkspace/playground/antlr/src/main/java/zhengwei/antlr/g4\ArrayInit.g4 by ANTLR 4.9.1
+package zhengwei.antlr.arrayinit;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link ArrayInitParser}.
+ */
+public interface ArrayInitListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link ArrayInitParser#init}.
+	 * @param ctx the parse tree
+	 */
+	void enterInit(ArrayInitParser.InitContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ArrayInitParser#init}.
+	 * @param ctx the parse tree
+	 */
+	void exitInit(ArrayInitParser.InitContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link ArrayInitParser#value}.
+	 * @param ctx the parse tree
+	 */
+	void enterValue(ArrayInitParser.ValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link ArrayInitParser#value}.
+	 * @param ctx the parse tree
+	 */
+	void exitValue(ArrayInitParser.ValueContext ctx);
+}
