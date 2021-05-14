da61e1b8463e074c22a07ad8acae4387f9277b25
zenv <zw30522@live.com>
2021-05-14 09:25:02 +0800

    使用ANTLR v4 对 JSON to XML 转换

---
+++ antlr/src/main/java/zhengwei/antlr/jsontoxml/JSONVisitor.java
@@ -0,0 +1,81 @@
+// Generated from C:/Users/zw305/Desktop/MyWorkspace/playground/antlr/src/main/java/zhengwei/antlr/g4\JSON.g4 by ANTLR 4.9.1
+package zhengwei.antlr.jsontoxml;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link JSONParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface JSONVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link JSONParser#json}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitJson(JSONParser.JsonContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code AnObject}
+	 * labeled alternative in {@link JSONParser#object}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAnObject(JSONParser.AnObjectContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code EmptyObject}
+	 * labeled alternative in {@link JSONParser#object}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitEmptyObject(JSONParser.EmptyObjectContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code ArrayOfValues}
+	 * labeled alternative in {@link JSONParser#array}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArrayOfValues(JSONParser.ArrayOfValuesContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code EmptyArray}
+	 * labeled alternative in {@link JSONParser#array}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitEmptyArray(JSONParser.EmptyArrayContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link JSONParser#pair}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPair(JSONParser.PairContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code String}
+	 * labeled alternative in {@link JSONParser#value}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitString(JSONParser.StringContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code Atom}
+	 * labeled alternative in {@link JSONParser#value}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAtom(JSONParser.AtomContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code ObjectValue}
+	 * labeled alternative in {@link JSONParser#value}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitObjectValue(JSONParser.ObjectValueContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code ArrayValue}
+	 * labeled alternative in {@link JSONParser#value}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArrayValue(JSONParser.ArrayValueContext ctx);
+}
